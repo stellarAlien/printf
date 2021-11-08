0f2fd44989efd85e42026f20874626b54f9d44a1
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2021-11-08 16:46:06 +0100

    Printfunction

---
+++ printf.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_char - prints char from the va_list
+* print_string - prints a string parameter from a va_list
+* @ap: va_list from calling function
+* Return: integer count of characters printed
+*/
+int print_char(va_list ap)
+{
+  return (_putchar(va_arg(ap, int)));
+}
+int print_string(va_list ap)
+{
+  char *str = va_arg(ap, char *);
+  int count = 0;
+  
+  if (!str)
+     str = "(null)";
+  while (str[count] != '\0')
+    count += _putchar(str[count]);
+  
+  return (count);
+} // by Omar Cherni & Saif Gaida
