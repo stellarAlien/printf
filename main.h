27568ae0e3620306f4fc96f40f38a51d80912cb6
Omar <89669057+dreamdaddybanks@users.noreply.github.com>
2021-11-08 15:07:16 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
-#ifndef MAIN_H
+#ifindef MAIN_H
 #define MAIN_H
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+
+/**
+ * struct printf - struct printf
+ * @c: The character
+ * @f: The function associated
+ */
+typedef struct printf
+{
+	char c;
+	int (*fun)(va_list);
+} printf_t;
 int _printf(const char *format, ...);
-#endif
+
+#endif //MAIN_H
